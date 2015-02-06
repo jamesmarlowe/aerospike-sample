8222c8a498b2ed9371ddb8c07377628559712038
James Marlowe <james.marlowe@lumate.com>
2015-02-06 04:19:03 -0600

    adding base files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # aerospike-sample
 Sample cpp project for testing a connection to an aerospike database
+
+Requirements
+------------
+```
+sudo apt-get install libssl0.9.8 libssl-dev liblua5.1-dev
+sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.1.so /usr/lib/liblua.so
+sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.1.a /usr/lib/liblua.a
+wget http://www.aerospike.com/download/client/c/3.1.0/artifact/ubuntu12
+tar xvzf aerospike-client-c-3.1.0.ubuntu12.04.x86_64.tgz
+sudo dpkg -i aerospike-client-c-devel-3.1.0.ubuntu12.04.x86_64.deb
+```
+
+build
+-----
+```
+git clone git@github.com:jamesmarlowe/aerospike-sample.git
+cd aerospike-sample
+mkdir build
+cd build
+cmake ..
+```
---
+++ client.cpp
@@ -0,0 +1,77 @@
+//
+/// \file client.cpp
+/// \author James Marlowe
+/// \brief Functions related to interacting with aerospike
+//
+
+#include <string>
+#include <iostream>
+#include <aerospike/aerospike.h>
+#include <aerospike/aerospike_key.h>
+#include <aerospike/as_error.h>
+#include <aerospike/as_record.h>
+#include <aerospike/as_status.h>
+
+std::string get_cache(aerospike &as, std::string &snamespace, std::string &sset, std::string &skey, std::string &sbin)
+{
+    as_error err;
+
+    //Initialize Key
+    as_key key;
+    as_key_init(&key, snamespace, sset, skey);
+
+    //Read from database
+    as_record* p_rec = NULL;
+    static const char* select[] = { sbin, NULL };
+    if (aerospike_key_select(&as, &err, NULL, &key, select, &p_rec) != AEROSPIKE_OK) {
+        fprintf(stderr, "err(%d) %s at [%s:%d]\n", err.code, err.message, err.file, err.line);
+    }
+    char * value = as_record_get_str(rec, bin);
+    return value
+    as_record_destroy(rec);
+}
+
+void set_cache(aerospike &as, std::string &snamespace, std::string &sset, std::string &skey, std::string &sbin, std::string &svalue)
+{
+    as_error err;
+    
+    //Initialize Key
+    as_key key;
+    as_key_init(&key, snamespace, sset, skey);
+    
+    //Initialize Record Data
+    as_record rec;
+    as_record_inita(&rec, 1);
+    as_record_set_str(&rec, sbin, svalue);
+
+    //Write to Database
+    if (aerospike_key_put(&as, &err, NULL, &key, rec) != AEROSPIKE_OK) {
+        fprintf(stderr, "err(%d) %s at [%s:%d]\n", err.code, err.message, err.file, err.line);
+    }
+}
+
+int main ()
+{
+    //Configure the Client
+    as_config config;
+    as_config_init(&config);
+    config.hosts[0] = { .addr = "127.0.0.1", .port = 3000 };
+    
+    //Initialize a Client
+    aerospike as;
+    aerospike_init(&as, &config);
+    
+    //Establish a Connection
+    as_error err;
+    if (aerospike_connect(&as, &err) != AEROSPIKE_OK) {
+        fprintf(stderr, "err(%d) %s at [%s:%d]\n", err.code, err.message, err.file, err.line);
+    }
+    
+    // set a value
+    set_cache(as, "test", "seconds", "test_key", "time", "1234");
+    
+    // set a value
+    std::string value = get_cache(as, "test", "seconds", "test_key", "time");
+    
+    return 0;
+}
